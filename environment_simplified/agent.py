d14c3f1a392b32bfddf8b662e2b695efed8ad1d4
QiL39 <chen.qil@northeastern.edu>
2024-07-22 17:47:59 -0400

    The demo of RL training and testing with 2 options (no history and last actions), using A2C.

--- environment_simplified/agent.py
+++ environment_simplified/agent.py
@@ -1,5 +1,9 @@
 from utils.provider import Provider
 from collections import deque
+from utils.model import ActorCritic
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class LLMAgent():
     def __init__(self, setting_file, agent_name):
@@ -28,10 +32,58 @@ class LLMAgent():
     def answer(self, prompt):
         # Update memory
         self.memory['prompt'].append(prompt)
-
         # Query llm
         llm_actions = self.provider.query_llm(prompt)
 
         self.memory['llm_actions'].append(llm_actions.content)
 
         return llm_actions.content
+
+
+class A2C_manager():
+    def __init__(self, state_dim, action_dim, actor_lr=0.001, critic_lr=0.005, gamma=0.99):
+        self.gamma = gamma
+        self.actor_critic = ActorCritic(state_dim, action_dim)
+        self.optimizer_actor = optim.Adam(self.actor_critic.actor.parameters(), lr=actor_lr)
+        self.optimizer_critic = optim.Adam(self.actor_critic.critic.parameters(), lr=critic_lr)
+        self.loss_fn = nn.MSELoss()
+
+    def choose_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0)
+        action_probs, _ = self.actor_critic(state)
+        action = torch.multinomial(action_probs, 1).item()
+        return action
+
+    def train(self, state, action, reward, next_state, done):
+        state = torch.FloatTensor(state).unsqueeze(0)
+        next_state = torch.FloatTensor(next_state).unsqueeze(0)
+        action = torch.LongTensor([action])
+        reward = torch.FloatTensor([reward])
+        done = torch.FloatTensor([done])
+
+        _, state_value = self.actor_critic(state)
+        _, next_state_value = self.actor_critic(next_state)
+
+        expected_value = reward + (1 - done) * self.gamma * next_state_value
+        advantage = expected_value - state_value
+
+        action_probs, _ = self.actor_critic(state)
+        action_log_probs = torch.log(action_probs.squeeze(0)[action])
+
+        actor_loss = -action_log_probs * advantage.detach()
+        critic_loss = self.loss_fn(state_value, expected_value.detach())
+
+        self.optimizer_actor.zero_grad()
+        self.optimizer_critic.zero_grad()
+        actor_loss.backward()
+        critic_loss.backward()
+        self.optimizer_actor.step()
+        self.optimizer_critic.step()
+
+    def save_model(self, path):
+        torch.save(self.actor_critic.state_dict(), path)
+
+    def load_model(self, path):
+        self.actor_critic.load_state_dict(torch.load(path))
+        self.actor_critic.eval()
+
---
+++ environment_simplified/utils/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def convert_int_to_str(action):
+
+    if action == 0:
+        return "D"
+    elif action == 1:
+        return "C"
+    else:
+        print(f'Invalid action: {action}')
+        return -1
+
+def convert_str_to_int(action):
+    if action == "D":
+        return 0
+    elif action == "C":
+        return 1
+    else:
+        print(f'Invalid action: {action}')
+        return -1
+
+def get_reward(action):
+    if action == "C":
+        return 1
+    elif action == "D":
+        return 0
+    else:
+        print(f'Invalid action: {action}')
+        return None
+
+def stat_analysis(all_actions):
+    # this list is a list of C and D actions.
+    # count them and print the ration of C and D.
+    np_actions = np.array(all_actions)
+    print(f'Total actions: {len(all_actions)}')
+    print(f'Total C: {np.sum(np_actions == "C")}')
+    print(f'Total D: {np.sum(np_actions == "D")}')
+    print(f'Ratio of C: %{round(np.sum(np_actions == "C") / len(all_actions), 2) * 100}')
+    print(f'Ratio of D: %{round(np.sum(np_actions == "D") / len(all_actions), 2) * 100}')
+    print("--------------------")
+
+def get_C_ratio(all_actions):
+    np_actions = np.array(all_actions)
+    C_ratio = round(np.sum(np_actions == "C") / len(all_actions), 2)
+
+    return C_ratio
