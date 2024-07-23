8986024734de9dbe1a3f377a8825b8486165208c
sepehrilami <sepehrilami@yahoo.com>
2024-07-23 17:11:55 -0400

    ,icro-validation with multiple qualitative adjectives

--- environment_simplified/micro_validation.py
+++ environment_simplified/micro_validation.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import PromptTemplate
 
 start_time = datetime.now()
 num_agent = 2
-game_length = 100
+game_length = 50
 
 # Load settings
 prompts_file = json.load(open('settings/prompts.json'))
@@ -17,8 +17,8 @@ prompt_option_no_history_template = prompts_file['prompt_no_history']
 prompt_option_markovian_history_template = prompts_file['prompt_markovian_history']
 
 multiple_input_prompt = PromptTemplate(
-    input_variables=['your_action', 'other_action'],
-    template=prompt_option_markovian_history_template
+    input_variables=['own_frequence', 'neighbors_frequence'],
+    template=prompt_option_no_history_template
 )
 
 
@@ -26,23 +26,23 @@ llm_agent_list = [LLMAgent(env_settings, 'llm_agent'+str(i)) for i in range(num_
 
 initial_time = time.time()
 # outer loop for rounds
+
+
 all_actions1 = []
 all_actions2 = []
+freq1 = 'occasionally'
+freq2 = 'sometimes'
+print(f'Agent 1: {freq1}, Agent 2: {freq2}')
 for step in range(game_length):
-    # print(step)
-    prompt_1 = prompts_file['prompt_no_history']
-    prompt_2 = prompts_file['prompt_no_history']
-    # prompt_1 = multiple_input_prompt.format(your_action="C", other_action="C")
-    # prompt_2 = multiple_input_prompt.format(your_action="C", other_action="C")
+    prompt_1 = multiple_input_prompt.format(own_frequence=freq1, neighbors_frequence=freq2)
+    # prompt_2 = multiple_input_prompt.format(own_frequence=freq2, neighbors_frequence=freq1)
 
     action_1 = llm_agent_list[0].answer(prompt_1)
-    action_2 = llm_agent_list[1].answer(prompt_2)
-    # print(action_1, action_2)
+    # action_2 = llm_agent_list[1].answer(prompt_2)
     all_actions1.append(action_1)
-    all_actions2.append(action_2)
-    # inner loop for agents
+    # all_actions2.append(action_2)
 stat_analysis(all_actions1)
-stat_analysis(all_actions2)
+# stat_analysis(all_actions2)
 
 print(f'Total time: {round(time.time() - initial_time, 2)}')
 
