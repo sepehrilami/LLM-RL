f3766b7349095bc106a26f94f606b6f09787a831
sepehrilami <sepehrilami@yahoo.com>
2024-07-31 17:08:00 -0400

    Micro validation - new prompt - scenario automation - etc

--- environment_simplified/micro_validation.py
+++ environment_simplified/micro_validation.py
@@ -8,18 +8,18 @@ from langchain_core.prompts import PromptTemplate
 
 start_time = datetime.now()
 num_agent = 2
-game_length = 50
+game_length = 100
 
 # Load settings
 prompts_file = json.load(open('settings/prompts.json'))
 env_settings = json.load(open('settings/env_settings.json'))
-prompt_option_no_history_template = prompts_file['prompt_no_history']
-prompt_option_markovian_history_template = prompts_file['prompt_markovian_history']
+# prompt_option_no_history_template = prompts_file['prompt_no_history']
+# prompt_option_markovian_history_template = prompts_file['prompt_markovian_history']
 
-multiple_input_prompt = PromptTemplate(
-    input_variables=['own_frequence', 'neighbors_frequence'],
-    template=prompt_option_no_history_template
-)
+# multiple_input_prompt = PromptTemplate(
+#     input_variables=['own_frequence', 'neighbors_frequence'],
+#     template=prompt_option_no_history_template
+# )
 
 
 llm_agent_list = [LLMAgent(env_settings, 'llm_agent'+str(i)) for i in range(num_agent)]
@@ -29,19 +29,34 @@ initial_time = time.time()
 
 
 all_actions1 = []
-all_actions2 = []
-freq1 = 'occasionally'
-freq2 = 'sometimes'
-print(f'Agent 1: {freq1}, Agent 2: {freq2}')
-for step in range(game_length):
-    prompt_1 = multiple_input_prompt.format(own_frequence=freq1, neighbors_frequence=freq2)
-    # prompt_2 = multiple_input_prompt.format(own_frequence=freq2, neighbors_frequence=freq1)
+# all_actions2 = []
+# freq1 = 'occasionally'
+# freq2 = 'sometimes'
+# print(f'Agent 1: {freq1}, Agent 2: {freq2}')
+print('Start playing...')
+all_keys = ['goal_several_long_run', 'goal_several_long_run_CoT']
+prompt_list = [prompts_file[key] for key in all_keys]
+for i, prompt_1 in enumerate(prompt_list):
+    all_actions1 = []
+    filename = f'all_actions_{all_keys[i]}'
+    print(f'Starting prompt: {all_keys[i]}')
+    for step in range(game_length):
+        # prompt_1 = prompts_file['prompt_no_history']
+        # prompt_1 = multiple_input_prompt.format(own_frequence=freq1, neighbors_frequence=freq2)
+        # prompt_2 = multiple_input_prompt.format(own_frequence=freq2, neighbors_frequence=freq1)
 
-    action_1 = llm_agent_list[0].answer(prompt_1)
-    # action_2 = llm_agent_list[1].answer(prompt_2)
-    all_actions1.append(action_1)
+        action_1 = llm_agent_list[0].answer(prompt_1)
+        # action_2 = llm_agent_list[1].answer(prompt_2)
+        all_actions1.append(action_1)
+        
+        with open(f'results/{filename}.json', 'w') as f:
+                json.dump(all_actions1, f)
+            
+        if step % 10 == 0:            
+            time.sleep(8)
+    
     # all_actions2.append(action_2)
-stat_analysis(all_actions1)
+    stat_analysis(all_actions1)
 # stat_analysis(all_actions2)
 
 print(f'Total time: {round(time.time() - initial_time, 2)}')
